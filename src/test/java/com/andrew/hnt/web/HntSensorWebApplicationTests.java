0814dfac69df7086634d14236e9cc38055d0220f
andrew <andrew2767@gmail.com>
2022-07-20 00:29:28 +0900

    initial commit

---
+++ src/test/java/com/andrew/hnt/web/HntSensorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.hnt.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HntSensorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
